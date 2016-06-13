c8af5b8c5782205366626f086bf3771bbec17305
Kiriu Tomoaki <tk9510159@gmail.com>
2016-06-13 13:47:17 +0900

    add login.html & modify files

--- phpBoard.php
+++ phpBoard.php
@@ -5,26 +5,25 @@
 <title>board</title>
 </head>
 <body>
-  <h1>Todolist</h1>
-  <form method="GET" action="./phpBoard.php">
-  <table border="1"　width="500" cellspacing="0" cellpadding="5" id="table1">
-  <tr>
-    <td><input type="text" name="name" size="30" maxlength="20" required></td>
-    <td>
-      <input type="hidden" name="login" value="1">
-      <input type="submit" value="login"></td>
-  </tr>
-</form>
+  <a style='float:right;' href='./login.html'>ログアウト</a>
+  <h1>掲示板</h1>
+  <form method='POST' action='./phpBoard.php'>
+    <p><?php print($_POST['user']."さん"); ?></p>
+    <input type='text' name='name'>
+    <input type='submit' value='add'>
+    <input type='hidden' name='add' value='1'>
+  </form>
+  <hr>
   <?php
     $db = new PDO('mysql:host=localhost;dbname=Boardlist;charset=utf8', 'root', '');
-    if(isset($_GET['login'])){
+    if(isset($_POST['login'])){
       $stt = $db->prepare('INSERT INTO board (user) VALUES (:user)');
-      $stt->bindParam(':user', $_GET['name'], PDO::PARAM_STR);
+      $stt->bindParam(':user', $_POST['name'], PDO::PARAM_STR);
       $stt->execute();
       //画面遷移する。
-    }else if(isset($_GET['delete'])){
+    }else if(isset($_POST['delete'])){
       $stt = $db->prepare('DELETE FROM board WHERE id = :id');
-      $stt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+      $stt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
       $stt->execute();
     }
 
@@ -36,14 +35,14 @@
       <p><?php print($row['user']);
                print(" ");
                print($row['date']);?>
-       <form method="GET" action="./phpBoard.php">
+       <form method="POST" action="./phpBoard.php">
          <input type="image" src="ico_ashcan1_9.gif" name="delete" alt="削除">
          <input type="hidden" name="delete" value="1">
          <input type="hidden" name="id" value="<?php print($row['id']); ?>">
        </form>
         <?php
         print("\n");
-        print($row['text']);?>
+        print($row['name']);?>
       </p>
   </tr>
 
