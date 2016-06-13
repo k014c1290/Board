2100425b24845a9d642226ab5179c09c86d8add5
Kiriu Tomoaki <tk9510159@gmail.com>
2016-06-13 14:17:49 +0900

    modify phpBoard.php

--- phpBoard.php
+++ phpBoard.php
@@ -12,15 +12,18 @@
     <input type='text' name='name'>
     <input type='submit' value='add'>
     <input type='hidden' name='add' value='1'>
+    <input type='hidden' name='user' value='<?php print($_POST['user']); ?>'>
   </form>
   <hr>
   <?php
     $db = new PDO('mysql:host=localhost;dbname=Boardlist;charset=utf8', 'root', '');
-    if(isset($_POST['login'])){
-      $stt = $db->prepare('INSERT INTO board (user) VALUES (:user)');
-      $stt->bindParam(':user', $_POST['name'], PDO::PARAM_STR);
+    if(isset($_POST['add'])){
+      $stt = $db->prepare("INSERT INTO board (user, date, name) VALUES (:user, STR_TO_DATE(:date, '%Y/%m/%d'), :name)");
+      $now = date('Y/m/d');
+      $stt->bindParam(':user', $_POST['user'], PDO::PARAM_STR);
+      $stt->bindParam(':date', $now, PDO::PARAM_STR);
+      $stt->bindParam(':name', $_POST['name'], PDO::PARAM_STR);
       $stt->execute();
-      //画面遷移する。
     }else if(isset($_POST['delete'])){
       $stt = $db->prepare('DELETE FROM board WHERE id = :id');
       $stt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
@@ -32,20 +35,18 @@
     while($row = $stt->fetch()){
   ?>
   <tr>
-      <p><?php print($row['user']);
-               print(" ");
-               print($row['date']);?>
-       <form method="POST" action="./phpBoard.php">
-         <input type="image" src="ico_ashcan1_9.gif" name="delete" alt="削除">
-         <input type="hidden" name="delete" value="1">
-         <input type="hidden" name="id" value="<?php print($row['id']); ?>">
-       </form>
-        <?php
-        print("\n");
-        print($row['name']);?>
-      </p>
+    <p>
+      <form method="POST" action="./phpBoard.php">
+        <?php print($row['user']." ".$row['date']); ?>
+        <input type="image" src="ico_ashcan1_9.gif" name="delete" alt="削除">
+        <input type="hidden" name="delete" value="1">
+        <input type="hidden" name="id" value="<?php print($row['id']); ?>">
+        <input type='hidden' name='user' value='<?php print($_POST['user']); ?>'>
+        <p><?php print($row['name']); ?></p>
+      </form>
+    </p>
+    <hr>
   </tr>
-
 <?php } ?>
 </body>
 </html>
