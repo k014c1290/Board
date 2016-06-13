0831e0c380edee47d4f066f66343f7e052d4371b
町田 <k014c1298@it-neec.jp>
2016-06-13 11:55:40 +0900

    repair

--- createdb.sh
+++ createdb.sh
@@ -1,4 +1,4 @@
 SQL=/opt/lampp/bin/mysql
 
 $SQL -u root -e "CREATE DATABASE Boardlist DEFAULT CHARACTER SET utf8;"
-$SQL -u root -e "CREATE TABLE Todolist.todo (user TEXT, date DATE, id INT NOT NULL AUTO_INCREMENT, name TEXT );"
+$SQL -u root -e "CREATE TABLE Boardlist.board (user TEXT, date DATE, id INT NOT NULL AUTO_INCREMENT, name TEXT ,PRIMARY KEY(id));"
