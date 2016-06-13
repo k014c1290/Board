9834d0bd6b046d2710879615626812d2967b2e51
町田 <k014c1298@it-neec.jp>
2016-06-13 11:16:07 +0900

    database

---
+++ phpBoard.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>board</title>
+</head>
+<body>
+  <h1>Todolist</h1>
+  <form method="GET" action="./phpBoard.php">
+  <table border="1"　width="500" cellspacing="0" cellpadding="5" id="table1">
+  <tr>
+    <td><input type="text" name="name" size="30" maxlength="20" required></td>
+    <td>
+      <input type="hidden" name="login" value="1">
+      <input type="submit" value="login"></td>
+  </tr>
+</form>
+  <?php
+  try{
+    $db = new PDO('mysql:host=localhost;dbname=Boardlist;charset=utf8', 'root', '');
+}
+catch{
+  print("unko");
+}
+    if(isset($_GET['login'])){
+      $stt = $db->prepare('INSERT INTO board (user) VALUES (:user)');
+      $stt->bindParam(':user', $_GET['name'], PDO::PARAM_STR);
+      $stt->execute();
+      //画面遷移する。
+    }else if(isset($_GET['delete'])){
+      $stt = $db->prepare('DELETE FROM board WHERE id = :id');
+      $stt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+      $stt->execute();
+    }
+
+    $stt = $db->prepare('SELECT * FROM board');
+    $stt->execute();
+    while($row = $stt->fetch()){
+  ?>
+  <tr>
+    <td class="task">
+      <p><?php print($row['user']);
+               print($row['date']);?>
+       <form method="GET" action="./phpBoard.php">
+         <input type="image" src="ico_ashcan1_9.gif" name="delete" alt="削除">
+         <input type="hidden" name="delete" value="1">
+         <input type="hidden" name="id" value="<?php print($row['id']); ?>">
+       </form>
+      </p>
+      <p><?php print($row['text']);?>
+      </p>
+
+    </td>
+  </tr>
+
+<?php } ?>
+</body>
+</html>
